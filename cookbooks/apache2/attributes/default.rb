1d4d468f1fe6e47cd40e0e33555d5c94129958c5
Tiejun Li <tieli@ymail.com>
2015-10-14 14:56:44 -0700

    Update apache2 listening port

--- cookbooks/apache2/attributes/default.rb
+++ cookbooks/apache2/attributes/default.rb
@@ -267,7 +267,7 @@ if node['apache']['service_name'].nil?
   default['apache']['service_name'] = node['apache']['package']
 end
 default['apache']['listen_addresses']  = %w(*)
-default['apache']['listen_ports']      = %w(80)
+default['apache']['listen_ports']      = %w(8080)
 default['apache']['contact']           = 'ops@example.com'
 default['apache']['timeout']           = 300
 default['apache']['keepalive']         = 'On'
