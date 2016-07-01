139a6115eb2c9eb2181a105a3a1a7252a95edd5a
Tiejun Li <tieli@ymail.com>
2016-07-01 00:11:36 -0700

    update rc files.

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -22,3 +22,13 @@ include_recipe 'main::package'
 # python_pip 'Django' do
 #    version '1.9'
 # end
+#
+
+# update rc files
+rc_files = %w{.bashrc .bash_aliases .ansible.cfg .gemrc .irbrc .railsrc .screenrc}
+rc_files.each do |file|
+   template "/home/#{node[:user][:name]}/#{file}" do
+     erb_file = file[1..-1]
+     source "#{erb_file}.erb"
+   end
+end
